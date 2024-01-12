bc33220186cac00c76798a8910a777834063fb35
Ali Abbasi <ali_sitrick@yahoo.com>
2024-01-12 14:28:47 -0800

    truncated update processing code

--- updateProcessor.php
+++ updateProcessor.php
@@ -22,35 +22,35 @@ if (!isset($_POST['id']) && !isset($_POST['firstname']) && !isset($_POST['lastna
         header("Location: update.php?id=$formerId");
         die();
     } else {
+
+        require_once "dbinfo.php";
+        $mysqli = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+        if ($mysqli->connect_errno) {
+            die("<p>Could not connect to DB: " . $mysqli->connect_error . "</p>");
+        }
+
         if (strlen($id) != 9) {
             $_SESSION['errorMessages'] = "<p class='error-message'>Please enter a valid student number...</p>";
             header("Location: update.php?id=$formerId");
             die();
         } else if ($id != $formerId) {
-            require_once "dbinfo.php";
-            $mysqli = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
-            if ($mysqli->connect_errno) {
-                die("<p>Could not connect to DB: " . $mysqli->connect_error . "</p>");
-            }
             if ($mysqli->query("SELECT id FROM students WHERE id = '$id'")->num_rows > 0) {
                 $_SESSION['errorMessages'] = "<p class='error-message'>Sorry, this student already exists...</p>";
                 header("Location: update.php?id=$formerId");
                 die();
             }
         }
-        require_once "dbinfo.php";
-        $mysqli = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
-        if ($mysqli->connect_errno) {
-            die("<p>Could not connect to DB: " . $mysqli->connect_error . "</p>");
-        }
+
         $escaped_id = $mysqli->real_escape_string($id);
         $escaped_firstname = $mysqli->real_escape_string($firstname);
         $escaped_lastname = $mysqli->real_escape_string($lastname);
+
         $query = "UPDATE students SET id = '$escaped_id', firstname = '$escaped_firstname', lastname = '$escaped_lastname' WHERE id = '$formerId'";
         $result = $mysqli->query($query);
         if (!$result) {
             die("<p>Could not update DB: " . $mysqli->error . "</p>");
         }
+        $mysqli->close();
         $_SESSION['successMessage'] = "<p class='success-message'>" . $firstname . " " . $lastname . " was updated successfully!</p>";
         header("Location: index.php");
         die();
